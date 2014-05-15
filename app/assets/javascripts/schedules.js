0b8d411e6f1d28b601e82b2d1805e7e7034f821c
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2014-05-14 19:34:11 -0700

    better navigation links; color current day of the week on schedules index; only list the day on the first days comment on comments index

--- app/assets/javascripts/schedules.js
+++ app/assets/javascripts/schedules.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(document).on('click', '#scheduled_event_form_revealer', function(){
-    $('#scheduled_event_form').toggle();
+    $('#scheduled_event_form_container').toggle();
   });
   $(document).on('click', '.fake_link', function(){ return false; });
 });
